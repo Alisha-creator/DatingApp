793da8a75c5cf0bad9112bf4a0930b2dd0010abf
Alisha-creator <alisha333165@gmail.com>
2023-06-28 08:46:34 +0530

    Added register and login functionality client side

---
+++ client/src/app/regirter/regirter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regirter',
+  templateUrl: './regirter.component.html',
+  styleUrls: ['./regirter.component.css']
+})
+export class RegirterComponent {
+
+}
