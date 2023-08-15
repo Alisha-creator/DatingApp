71a4a57db5238f1f93d1da92fbcf3a32e14edaaa
Alisha-creator <alisha333165@gmail.com>
2023-08-16 01:42:42 +0530

    Adding the Like User feature

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -30,8 +30,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 }
                 throw modalStateErrors.flat();
               }
-              else {
+              else if (typeof(error.error) === 'object') {
                 this.toastrService.error(error.statusText, error.status);
+              }
+              else {
+                this.toastrService.error(error.error, error.status);
               }
               break;
 
